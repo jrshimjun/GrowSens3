8d4438def6da4f56ae4207d1d69dd02db1cf3859
Minjun Shim <143543359+jrshimjun@users.noreply.github.com>
2025-03-04 22:40:01 -0500

    Add files via upload
    Base app commit

---
+++ CameraView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+struct CameraView: View {
+    var body: some View {
+        VStack {
+            Text("Light Sensor Coming Soon...")
+                .font(.title)
+                .padding()
+        }
+        .navigationTitle("Light Sensor")
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
+
---
+++ New File.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            Text("Settings Coming Soon...")
+                .font(.title)
+                .padding()
+        }
+        .navigationTitle("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
