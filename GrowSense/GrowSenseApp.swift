86179a4aaa29f08bff0b6fb9800072369fbc75a3
Jason Park <141460876+Sa3JPark@users.noreply.github.com>
2025-03-10 19:52:15 -0400

    Initial Commit

---
+++ GrowSense/GrowSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowSenseApp.swift
+//  GrowSense
+//
+//  Created by Jason Park on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GrowSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GrowSenseTests/GrowSenseTests.swift
@@ -0,0 +1,16 @@
+//
+//  GrowSenseTests.swift
+//  GrowSenseTests
+//
+//  Created by Jason Park on 3/10/25.
+//
+
+import Testing
+
+struct GrowSenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
